8780d7a433630b94d0ec202583ee295ef3308883
Maxime Cousseau <coussmax@coussmax.home>
2018-11-18 21:09:04 +0100

    Ajout de la classe 'Client' et de son header

---
+++ Client.cpp
@@ -0,0 +1,8 @@
+//
+//  Client.cpp
+//  
+//
+//  Created by Maxime Cousseau on 18/11/2018.
+//
+
+#include "Client.hpp"
---
+++ Client.hpp
@@ -0,0 +1,13 @@
+//
+//  Client.hpp
+//  
+//
+//  Created by Maxime Cousseau on 18/11/2018.
+//
+
+#ifndef Client_hpp
+#define Client_hpp
+
+#include <stdio.h>
+
+#endif /* Client_hpp */
