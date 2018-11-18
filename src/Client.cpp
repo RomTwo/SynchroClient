5239964e9968b26dfca3f39a9ade1f85191e180d
Maxime Cousseau <coussmax@coussmax.home>
2018-11-19 00:01:16 +0100

    Modification de la classe 'Client'. Ajout de la fonction 'existProfil'.

--- src/Client.cpp
+++ src/Client.cpp
@@ -7,8 +7,76 @@
 
 #include "Client.hpp"
 #include <iostream>
+#include <string>
+using namespace std;
+
+enum Command{
+    CREATE,
+    EDIT,
+    LOAD
+};
+
+Client::Client(){
+    
+}
+
+void Client::createProfil(const string& name){
+    cout << "createProfil avec le nom " << name << endl;
+    if(this->existProfil(name)){
+        cout << "Yes" << endl;
+    }else{
+        cout << "Nop" << endl;
+    }
+}
+
+void Client::editProfil(){
+    cout << "editProfil\n";
+}
+
+void Client::loadProfil(){
+    cout << "loadProfil\n";
+}
+
+bool Client::existProfil(const string& name){
+    DIR *dir;
+    struct dirent *entry;
+    struct stat info;
+    bool reponse;
+    
+    dir = opendir("../configuration");
+    if(!dir){
+        cout << "Le dossier n'est pas existant" << endl;
+        return false;
+    }
+    
+    cout << "Le dossier existe bien.\nRecherche du fichier en cours" << endl;
+    while ((entry = readdir(dir)) != NULL) {
+        string fname = dirp->d_name;
+        if((entry->d_name) == name){
+            reponse = true;
+        }else{
+            reponse = false;
+        }
+        printf ("%s\n", entry->d_name);
+    }
+    closedir (dir);
+    return reponse;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string command = "", name, ip;
+    Client client;
+    while(command != "EXIT"){
+        cin >> command >> name;
+        if(command == "CREATE"){
+            client.createProfil(name);
+        }else if(command == "EDIT"){
+            client.editProfil();
+        }else if(command == "LOAD"){
+            client.loadProfil();
+        }else{
+            cout << "Commande non existante" << endl;
+        }
+    }
     return 0;
 }
--- src/Client.hpp
+++ src/Client.hpp
@@ -9,5 +9,19 @@
 #define Client_hpp
 
 #include <stdio.h>
+#include <string>
+#include <dirent.h>//Pour le directory
+#include <sys/stat.h>//Pour le directory
+using namespace std;
+
+class Client {
+    
+public:
+    Client();
+    void createProfil(const string& name);
+    void editProfil();
+    void loadProfil();
+    bool existProfil(const string& name);
+};
 
 #endif /* Client_hpp */
